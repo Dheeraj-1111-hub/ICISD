99c4e39f256e1ed009ec0b0603a24e5ec59c73df
Sai Dheeraj <ysaidheeraj1111@gmail.com>
2026-01-20 16:29:06 +0530

    Updated Committee by removing the images and added designation for organising secretaries

--- icisd-main/src/components/conference/Committee/CommitteeData.ts
+++ icisd-main/src/components/conference/Committee/CommitteeData.ts
@@ -1,167 +1,215 @@
 export const committeeData = {
   chiefPatrons: [
     {
-      name: "T. R. Paarivendhar",
+      name: "Dr. T. R. Paarivendhar",
       role: "Founder Chancellor",
       organization: "SRM Institute of Science and Technology",
-      img:"/dp.webp"
     },
     {
       name: "Dr. Ravi Pachamoothoo",
       role: "Pro-Chancellor (Admin)",
       organization: "SRM Institute of Science and Technology",
-      img:"https://srmuniversity.ac.in/uploads/51ba570fe68fc088e0a942bdf8700cdce7eb8b1d/1686912819DSC0753000-1.jpg"
     },
     {
       name: "Dr. P. Sathyanarayanan",
-      role: "Pro-Chancellor Academics",
+      role: "Pro-Chancellor (Academics)",
       organization: "SRM Institute of Science and Technology",
-      img:"https://www.srmap.edu.in/wp-content/uploads/2025/10/pro-chancellor.png"
     },
     {
       name: "Ms. Harini Ravi",
       role: "Director, Institutional Development",
       organization: "SRM Group",
-      img:"https://srmvalliammai.ac.in/wp-content/uploads/2023/01/ms.r.harini.jpeg"
     },
   ],
 
   patrons: [
-    {
-      name: "Dr. C. Muthamizhchelvan",
-      role: "Vice Chancellor (E&T)",
-      organization: "SRM Institute of Science and Technology",
-      img:"https://srmist.irins.org/profile_images/315472.jpg"
-    },
-    {
-      name: "Dr. S. Ponnusamy",
-      role: "Registrar",
-      organization: "SRM Institute of Science and Technology",
-      img:"https://srmrmp.edu.in/wp-content/uploads/2024/12/Dr.-S.-Ponnusamy.jpg"
-    },
-    {
-      name: "Dr. K. Gunasekaran",
-      role: "Controller of Examination",
-      organization: "SRM Institute of Science and Technology",
-      img:"https://lirp.cdn-website.com/5db65efd/dms3rep/multi/opt/dr-k-gunasekaran-640w.jpg"
-    },
-    {
-      name: "Dr. C. V. Jayakumar",
-      role: "Dean (FET)",
-      organization: "SRM Institute of Science and Technology Vadapalani Campus",
-      img:"https://srmistvdp.edu.in/wp-content/uploads/2022/07/dean-fet.jpg"
-    },
-    {
-      name: "Dr. C. Gomathy",
-      role: "Vice Principal (Academics & Placements)",
-      organization: "SRM Institute of Science and Technology Vadapalani Campus",
-      img:"https://srmistvdp.edu.in/wp-content/uploads/2022/07/gomathy-200x240-1.jpg"
-    },
-    {
-      name: "Dr. S. Karthikeyan",
-      role: "Vice Principal (Examinations)",
-      organization: "SRM Institute of Science and Technology Vadapalani Campus",
-      img:"https://srmistvdp.edu.in/wp-content/uploads/2022/07/vp-exams-200x240-1.jpg"
-    },
-    {
-      name: "Dr. K. Ramachandran",
-      role: "Research Coordinator",
-      organization: "SRM Institute of Science and Technology Vadapalani Campus",
-      img:"https://srmistvdp.edu.in/wp-content/uploads/2022/07/ramachandran.jpg"
-    },
-  ],
+  {
+    name: "Dr. C. Muthamizhchelvan",
+    role: "Vice Chancellor (E&T)",
+    organization: "SRM Institute of Science and Technology",
+  },
+  {
+    name: "Dr. S. Ponnusamy",
+    role: "Registrar",
+    organization: "SRM Institute of Science and Technology",
+  },
+  {
+    name: "Dr. K. Gunasekaran",
+    role: "Controller of Examinations",
+    organization: "SRM Institute of Science and Technology",
+  },
+  {
+    name: "Dr. R. Venkatesh Babu",
+    role: "Director (Academics and Administration)",
+    organization: "SRM Institute of Science and Technology",
+  },
+  {
+    name: "Dr. C. V. Jayakumar",
+    role: "Dean (FET)",
+    organization: "SRM Institute of Science and Technology, Vadapalani Campus",
+  },
+  {
+    name: "Dr. C. Gomathy",
+    role: "Vice Principal (Academics & Placements)",
+    organization: "SRM Institute of Science and Technology, Vadapalani Campus",
+  },
+  {
+    name: "Dr. S. Karthikeyan",
+    role: "Vice Principal (Examinations)",
+    organization: "SRM Institute of Science and Technology, Vadapalani Campus",
+  },
+  {
+    name: "Dr. K. Ramachandran",
+    role: "Research Coordinator",
+    organization: "SRM Institute of Science and Technology, Vadapalani Campus",
+  },
+],
+
 
   convener: [
     {
       name: "Dr. Golda Dilip",
-      organization: "Professor & HOD ,Department of CSE, SRMIST",
-      img:"https://srmistvdp.edu.in/wp-content/uploads/2024/03/golda-dilip.jpg"
+      role: "Professor & Head, Department of CSE",
+      organization:
+        "SRM Institute of Science and Technology, Vadapalani Campus",
     },
   ],
 
-  programChairs: [
+  programCoChairs: [
     {
-      name: "Name 1",
-      role: "Program Chair",
-      organization: "SRM Institute of Science and Technology",
-    },
-    {
-      name: "Name 2",
+      name: "Dr. Raj Ramachandran",
       role: "Program Co-Chair",
-      organization: "SRM Institute of Science and Technology",
+      organization:
+        "Senior Lecturer in Technologies (Head), Cardiff Metropolitan University, Cardiff, Wales, UK",
     },
   ],
 
   organisingSecretaries: [
     {
       name: "Dr. P. Durgadevi",
-      organization: "SRMIST",
-      img:"https://srmistvdp.edu.in/wp-content/uploads/2022/08/p-durga-devi-200x240-1.jpg",
+      role: "Assistant Professor (S.G), Department of CSE",
+      organization: "SRM Institute of Science and Technology",
     },
     {
       name: "Dr. H. Mary Shyni",
-      organization: "SRMIST",
-      img:"https://srmistvdp.edu.in/wp-content/uploads/2024/04/Dr.-H.-Mary-Shyni.png.webp",
+      role: "Assistant Professor (O.G), Department of CSE",
+      organization: "SRM Institute of Science and Technology",
     },
     {
-      name: "Ms. Charulatha .R .T",
-      organization: "SRMIST",
-      img:"https://srmistvdp.edu.in/wp-content/uploads/2024/04/Ms-Charulatha.R.T.png.webp",
+      name: "Ms. Charulatha R. T",
+      role: "Assistant Professor (Jr.G), Department of CSE",
+      organization: "SRM Institute of Science and Technology",
     },
   ],
 
-  steeringCommittee: Array.from({ length: 6 }).map((_, i) => ({
-    name: `Name ${i + 1}`,
-    role: "Steering Committee Member",
-    organization: "University / Organization",
-  })),
+  steeringCommittee: [
+    {
+      name: "Member Name 1",
+      role: "Steering Committee Member",
+      organization: "University / Organization",
+    },
+    {
+      name: "Member Name 2",
+      role: "Steering Committee Member",
+      organization: "University / Organization",
+    },
+    {
+      name: "Member Name 3",
+      role: "Steering Committee Member",
+      organization: "University / Organization",
+    },
+    {
+      name: "Member Name 4",
+      role: "Steering Committee Member",
+      organization: "University / Organization",
+    },
+    {
+      name: "Member Name 5",
+      role: "Steering Committee Member",
+      organization: "University / Organization",
+    },
+    {
+      name: "Member Name 6",
+      role: "Steering Committee Member",
+      organization: "University / Organization",
+    },
+  ],
 
-  programCommittee: Array.from({ length: 8 }).map((_, i) => ({
-    name: `Name ${i + 1}`,
-    role: "Program Committee Member",
-    organization: "University / Organization",
-  })),
+  programCommittee: [
+    {
+      name: "Member Name 1",
+      role: "Program Committee Member",
+      organization: "University / Organization",
+    },
+    {
+      name: "Member Name 2",
+      role: "Program Committee Member",
+      organization: "University / Organization",
+    },
+    {
+      name: "Member Name 3",
+      role: "Program Committee Member",
+      organization: "University / Organization",
+    },
+    {
+      name: "Member Name 4",
+      role: "Program Committee Member",
+      organization: "University / Organization",
+    },
+    {
+      name: "Member Name 5",
+      role: "Program Committee Member",
+      organization: "University / Organization",
+    },
+    {
+      name: "Member Name 6",
+      role: "Program Committee Member",
+      organization: "University / Organization",
+    },
+    {
+      name: "Member Name 7",
+      role: "Program Committee Member",
+      organization: "University / Organization",
+    },
+    {
+      name: "Member Name 8",
+      role: "Program Committee Member",
+      organization: "University / Organization",
+    },
+  ],
 
   organisingCommittee: [
     {
       name: "Dr. Rajasekar Velswamy",
-      organization: "SRMIST",
-      img:"https://srmistvdp.edu.in/wp-content/uploads/2024/03/rajasekar-300x277-1.jpg",
+      organization: "SRM Institute of Science and Technology",
     },
     {
       name: "Dr. G. Paavai Anand",
-      organization: "SRMIST",
-      img:"https://srmistvdp.edu.in/wp-content/uploads/2024/04/paavai-anand.jpg",
+      organization: "SRM Institute of Science and Technology",
     },
     {
       name: "Mrs. S. Niveditha",
-      organization: "SRMIST",
-      img:"https://srmistvdp.edu.in/wp-content/uploads/2022/08/nivedhitha-200x240-1.jpg",
+      organization: "SRM Institute of Science and Technology",
     },
     {
       name: "Dr. K. Karthikayani",
-      organization: "SRMIST",
-      img:"https://srmistvdp.edu.in/wp-content/uploads/2024/04/karthikayani-200x240-1.jpg",
+      organization: "SRM Institute of Science and Technology",
     },
     {
       name: "Dr. T. Anusha",
-      organization: "SRMIST",
-      img:"https://srmistvdp.edu.in/wp-content/uploads/2023/07/anusha-t-300x277-1.jpg",
+      organization: "SRM Institute of Science and Technology",
     },
     {
       name: "Mrs. M. Indumathy",
-      organization: "SRMIST",
-      img:"https://srmistvdp.edu.in/wp-content/uploads/2022/08/Ms-M.INDUMATHY-200x240-1.jpg",
+      organization: "SRM Institute of Science and Technology",
     },
     {
       name: "Mrs. Steffina Muthukumar",
-      organization: "SRMIST",
-      img:"https://srmistvdp.edu.in/wp-content/uploads/2022/08/Ms.-STEFFINA-MUTHUKUMAR-200x240-1.jpg",
+      organization: "SRM Institute of Science and Technology",
     },
     {
       name: "Dr. M. Kiruthiga Devi",
-      organization: "SRMIST",
-      img:"https://srmistvdp.edu.in/wp-content/uploads/2025/03/Screenshot-2025-03-03-090958.png.webp",
+      organization: "SRM Institute of Science and Technology",
     },
-  ]
+  ],
 };
